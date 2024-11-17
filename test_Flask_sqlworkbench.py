ccd1cd484ea91517b26aa2ec01156a4212b63b1d
Marcelo Manzano <202280097@pus.palawan.edu.ph>
2024-11-17 23:27:37 +0800

    testing create

--- test_Flask_sqlworkbench.py
+++ test_Flask_sqlworkbench.py
@@ -14,3 +14,25 @@ def test_client():
             yield client
             db.session.remove()
             db.drop_all()
+
+def test_get_all_students(test_client):
+    response = test_client.get('/api/students')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data['success'] is True
+    assert isinstance(data['data'], list)
+
+def test_create_student(test_client):
+    new_student = {
+        "student_name": "2022-8-0097",
+        "first_name": "Marcelo",
+        "last_name": "Manzano",
+        "middle_name": "R",
+        "sex": "Male",
+        "birthday": "2004-03-12"
+    }
+    response = test_client.post('/api/students', json=new_student)
+    assert response.status_code == 201
+    data = json.loads(response.data)
+    assert data['successs'] is True
+    assert data['data']['student_number'] == "2022-8-0097"
